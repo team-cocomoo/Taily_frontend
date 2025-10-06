8b62b1835102dbd299bac98d8173a85c92be91ca
beomjunkim-soul <kbg142337@naver.com>
2025-10-06 18:12:53 +0900

    fix: route 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 // App.js
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Container, Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// AuthProvider import
 import AuthProvider from "./contexts/AuthProvider";
-
-// 레이아웃 컴포넌트
 import HeaderNavbar from "./components/common/HeaderNavbar";
 import Footer from "./components/common/Footer";
-import WalkDiaryWritePage from "./pages/walkDiary/WalkDiaryWritePage";
 
 // 페이지 컴포넌트
+import WalkDiaryWritePage from "./pages/walkDiary/WalkDiaryWritePage";
 import TailyFriendsDetailPage from "./pages/tailyFriends/TailyFriendsDetailPage";
 import LoginPage from "./pages/user/LoginPage";
 import WalkPathMainPage from "./pages/walkPath/WalkPathMainPage";
@@ -22,41 +19,34 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        {/* <div className="App">
+        <div className="App">
           <HeaderNavbar />
           <Container className="mt-4">
             <Routes>
-              <Route path="/" element={<LoginPage />} /> */}
               {/* 로그인 페이지 */}
-
+              <Route path="/" element={<LoginPage />} />
               <Route path="/login" element={<LoginPage />} />
-              {/* 산책경로 페이지 */}
+
+              {/* 산책 경로 관련 */}
               <Route path="/walk-paths" element={<WalkPathMainPage />} />
-              {/* 산책 경로 상세 페이지 */}
               <Route path="/walkpath/:id" element={<WalkPathDetailPage />} />
-              {/* <Route path="/login" element={<LoginPage />} />
+
+              {/* 친구 상세 */}
               <Route
                 path="/taily-friends/:id"
                 element={<TailyFriendsDetailPage />}
               />
-            </Routes>
-          </Container>
-          <Footer />
-        </div> */}
-        <div className="App">
-          <HeaderNavbar />
-          <Container className="mt-4">
-            <WalkDiaryWritePage />
-            <Routes>
-              
+
+              {/* 산책 일지 작성 */}
+              <Route
+                path="/walk-diary/write"
+                element={<WalkDiaryWritePage />}
+              />
             </Routes>
           </Container>
           <Footer />
         </div>
       </AuthProvider>
-      {/* <AuthProvider> */}
-      {/* </AuthProvider> */}
-
     </BrowserRouter>
   );
 }
