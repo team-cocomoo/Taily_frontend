9d58b0a3ac625bbc61d859da6f2fceae4ea1f065
beomjunkim-soul <kbg142337@naver.com>
2025-10-09 18:52:05 +0900

    feat: 산책경로 title component 추가 및 위치 배열 정리

--- src/pages/walkPath/WalkPathWritePage.jsx
+++ src/pages/walkPath/WalkPathWritePage.jsx
@@ -1,35 +1,46 @@
 import React from "react";
 import { Form, Button, Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ImageBox from "../../components/board/ImageBox";
 import WalkPathContent from "../../components/board/walkPath/WalkPathContent";
-import "../../styles/walkPath/WalkPathWrite.css"
+import WalkPathTitle from "../../components/board/walkPath/WalkPathTitle";
+import "../../styles/walkPath/WalkPathWrite.css";
 
 const WalkPathWritePage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
 
-    return (
-        <div className="row justify-content-center">
-            {/* 산책 경로 - 지도 API */}
-             
+  return (
+    <div className="container main-content">
+      <div className="row justify-content-center">
+        <div className="col-12 col-lg-8">
+          {/* 제목 */}
+          <WalkPathTitle value={title} onChange={setTitle} />
 
-            {/*  내용 */}
-            <WalkPathContent />
+          {/* 산책 경로 - 지도 API */}
 
-            {/* 사진 첨부 */}
-            <ImageBox />
+          {/*  내용 */}
+          <WalkPathContent />
 
-            <div className="d-flex justify-content-center gap-2 mt-3">
-                <Button variant="secondary" onClick={() => navigate("/")}>목록</Button>
-                {/* type=submit */}
-                {/* <Button variant="primary" type="submit" disabled={loading}>
+          {/* 사진 첨부 */}
+          <ImageBox />
+
+          <div className="d-flex justify-content-center gap-2 mt-3">
+            <Button variant="secondary" onClick={() => navigate("/")}>
+              목록
+            </Button>
+            {/* type=submit */}
+            {/* <Button variant="primary" type="submit" disabled={loading}>
                     {loading ? "작성 중..." : "작성완료"}
                 </Button> */}
-                <Button variant="primary">완료</Button>
-            </div>
+            <Button variant="primary">완료</Button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default WalkPathWritePage;
