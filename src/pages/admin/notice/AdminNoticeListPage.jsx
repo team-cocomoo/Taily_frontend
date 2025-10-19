30561230682ce51ffc88ca924e7bd1687e31313e
GIS <gogointospace@gmail.com>
2025-10-20 06:50:07 +0900

    feat : 관리자 공지사항 구현중

---
+++ src/pages/admin/notice/AdminNoticeListPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdminNoticeListPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
