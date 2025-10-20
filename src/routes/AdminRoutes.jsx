213de1c098b4360b45ca7542ef1055d991a9c681
yey3hh <wateveryehwant@gmail.com>
2025-10-21 01:09:12 +0900

    feat: 관리자 회원 정보 조회 시 이미지 뜨도록 수정 중, css 추가

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -2,11 +2,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import AdminLayout from "@/layouts/AdminLayout";
-import ProtectedLayout from "@/components/common/ProtectedLayout";
+import AdminProtectedLayout from "@/components/common/AdminProtectedLayout";
 
 // 관리자 페이지
 import AdminLoginPage from "@/pages/admin/AdminLoginPage";
 import AdminMainPage from "@/pages/admin/AdminMainPage";
+import AdminUsersPage from "@/pages/admin/users/AdminUsersPage";
 import AdminFaqPage from "@/pages/admin/faq/AdminFaqPage";
 import AdminFaqDetailPage from "@/pages/admin/faq/AdminFaqDetailPage";
 import AdminFaqWriteAndEditPage from "@/pages/admin/faq/AdminFaqWriteAndEditPgae";
@@ -16,7 +17,6 @@ import AdminNoticeListPage from "@/pages/admin/notice/AdminNoticeListPage";
 import AdminNoticeDetailPage from "@/pages/admin/notice/AdminNoticeDetailPage";
 import AdminNoticeEditPage from "@/pages/admin/notice/AdminNoticeEditPage";
 import AdminNoticeWritePage from "@/pages/admin/notice/AdminNoticeWritePage";
-import Users from "@/components/admin/Users";
 
 const AdminRoutes = (
   <Route path="admin" element={<AdminLayout />}>
@@ -24,9 +24,9 @@ const AdminRoutes = (
     <Route path="login" element={<AdminLoginPage />} />
 
     {/* ROLE_ADMIN 접근 필요 */}
-    <Route element={<ProtectedLayout roles={["ROLE_ADMIN"]} />}>
-      <Route path="main" element={<AdminMainPage />}>
-        <Route path="users" element={<Users />} />
+    <Route element={<AdminProtectedLayout roles={["ROLE_ADMIN"]} />}>
+      <Route path="main/*" element={<AdminMainPage />}>
+        <Route path="users" element={<AdminUsersPage />} />
         <Route path="notices" element={<AdminNoticeListPage />} />
         <Route path="notices/:id" element={<AdminNoticeDetailPage />} />
         <Route path="notices/edit/:id" element={<AdminNoticeEditPage />} />
@@ -35,8 +35,8 @@ const AdminRoutes = (
         <Route path="inquiries" element={<AdminInquiryPage />} />
         <Route path="faqs" element={<AdminFaqPage />} />
         <Route path="faqs/:id" element={<AdminFaqDetailPage />} />
-        <Route path="/admin/main/faqs/write" element={<AdminFaqWriteAndEditPage />} />
-        <Route path="/admin/main/faqs/edit/:id" element={<AdminFaqWriteAndEditPage />} />
+        <Route path="faqs/write" element={<AdminFaqWriteAndEditPage />} />
+        <Route path="faqs/edit/:id" element={<AdminFaqWriteAndEditPage />} />
 
       </Route>
     </Route>
